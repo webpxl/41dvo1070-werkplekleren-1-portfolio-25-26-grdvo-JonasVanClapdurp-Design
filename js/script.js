7ef671ab445e5113639aad2a1f58794c0af14e74
Jonas Van Clapdurp <jonas.vanclapdurp@student.pxl.be>
2026-01-16 15:05:49 +0100

    - Added Characteristics and skills section

--- js/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrolled = window.pageYOffset;
-    const parallaxBg = document.querySelector('.parallax-bg');
-    parallaxBg.style.transform = `translateY(${scrolled * 0.5}px)`;
-});
